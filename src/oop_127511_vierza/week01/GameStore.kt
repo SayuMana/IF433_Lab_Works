9ef6bfdd1df34d6112510be3874bb4916df3d0a9
SayuMana <vierzasyahputra@gmail.com>
2026-02-09 16:09:49 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_127511_vierza/week01/GameStore.kt
+++ src/oop_127511_vierza/week01/GameStore.kt
@@ -1,6 +1,12 @@
 package oop_127511_vierza.week01
 
+fun calculateDiscount(price: Int): Int = if (price > 500000) {
+    (price * 0.2).toInt()
+} else {
+    (price * 0.1).toInt()
+}
+
 fun main() {
-    val gameTitle
-    val price
+    val gameTitle = "Tomb Raider"
+    val price = 125000
 }
