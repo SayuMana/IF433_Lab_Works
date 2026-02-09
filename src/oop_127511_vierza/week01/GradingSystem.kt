c390f77efd3a721225137f310e60f0026825f488
SayuMana <vierzasyahputra@gmail.com>
2026-02-09 15:33:45 +0700

    week01: refactor to use val and string templates

--- src/oop_127511_vierza/week01/GradingSystem.kt
+++ src/oop_127511_vierza/week01/GradingSystem.kt
@@ -1,8 +1,8 @@
 package oop_127511_vierza.week01
 
 fun main() {
-    var name = "SayuMana"
-    var score = 80
+    val name = "SayuMana"
+    val score = 80
 
     println("Nama: " + name + ", Nilai: " + score)
 }
