376602436d991e085c6d1a85b7366831c1a2cdc7
SayuMana <vierzasyahputra@gmail.com>
2026-02-11 15:43:57 +0700

    week02: (task 1) implement library input system

--- src/oop_127511_vierza/week2/tugasMandiri1/Loan.kt
+++ src/oop_127511_vierza/week2/tugasMandiri1/Loan.kt
@@ -8,9 +8,14 @@ class Loan (
     fun calculateFine(): Int {
         var denda = 0
 
-        if (loanDuration > 3) denda = (loanDuration - 3) * 2000
-        else if (loanDuration < 3) denda = 0
-
+        if (loanDuration > 3) {
+            denda = (loanDuration - 3) * 2000
+            println("Anda dikenakan denda: ${denda}")
+        }
+        else if (loanDuration < 3) {
+            denda = 0
+            println("Anda mengembalikan tepat waktu")
+        }
         return denda
     }
 }
--- src/oop_127511_vierza/week2/tugasMandiri1/Main.kt
+++ src/oop_127511_vierza/week2/tugasMandiri1/Main.kt
@@ -1,2 +1,29 @@
 package oop_127511_vierza.week2.tugasMandiri1
 
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    print("Masukkan Judul Buku: ")
+    val judul = scanner.nextLine()
+
+    print("Masukkan Nama Peminjam: ")
+    val borrower = scanner.nextLine()
+
+    print("Lama pinjam: ")
+    var loanDuration = scanner.nextInt()
+
+    scanner.nextLine()
+
+    if (loanDuration < 0) {
+        print("Lama pinjam (${loanDuration}) tidak boleh minus/0. Otomatis lama pinjam 1 hari")
+        loanDuration = 1
+    }
+
+    var loan = Loan(judul, borrower, loanDuration)
+    println("Judul Buku: ${loan.bookTitle}")
+    println("Peminjam: ${loan.borrower}")
+    println("Lama pinjam: ${loan.bookTitle}")
+    loan.calculateFine()
+}
