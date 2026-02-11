116f5f7fda798ceaf68e814637425ed1eeac576b
SayuMana <vierzasyahputra@gmail.com>
2026-02-11 16:22:23 +0700

    week02: (task 2) implement the game

--- src/oop_127511_vierza/week2/tugasMandiri2/Main.kt
+++ src/oop_127511_vierza/week2/tugasMandiri2/Main.kt
@@ -1,2 +1,49 @@
 package oop_127511_vierza.week2.tugasMandiri2
 
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    print("Masukkan Nama Hero: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan Base Damage: ")
+    val baseDamage = scanner.nextInt()
+
+    var hero = Hero(name, baseDamage)
+
+    var enemyName = "Menendez"
+    var enemyHP = 100
+    println("${enemyName} is attacking you!")
+
+    while (hero.isAlive() && enemyHP > 0) {
+        println("1. Attack")
+        println("2. Run")
+        print("Choose: ")
+        var choose = scanner.nextInt()
+
+        if (choose == 1) {
+            enemyHP -= baseDamage
+            println("Current Enemy's HP: ${enemyHP}")
+            if (enemyHP > 0) {
+                var randomDamage = (10..20).random()
+                hero.takeDamage(randomDamage)
+            }
+            println("Your hp: ${hero.hp}")
+        }
+
+        else if (choose == 2) break
+        else {
+            println("Invalid choice")
+        }
+    }
+
+    if (!hero.isAlive()) {
+        println("${hero.name} dead!")
+    } else if (enemyHP <= 0) {
+        println("${hero.name} killed ${enemyName}!")
+    } else {
+        println("${hero.name} is running away!")
+    }
+}
