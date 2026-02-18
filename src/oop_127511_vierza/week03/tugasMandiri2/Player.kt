5da62e26a4bf29f37f3adf9d43e065e3fd8765a1
SayuMana <vierzasyahputra@gmail.com>
2026-02-18 15:30:04 +0700

    week03: (task 2) create Player class with private XP

--- src/oop_127511_vierza/week03/tugasMandiri2/Player.kt
+++ src/oop_127511_vierza/week03/tugasMandiri2/Player.kt
@@ -1,4 +1,22 @@
 package oop_127511_vierza.week03.tugasMandiri2
 
 class Player {
+    var player: String = "Unknown"
+    private var xp: Int = 0
+    var level: Int = 0
+        get() = (xp / 100) + 1
+
+    fun addXP(amount: Int) {
+        if (amount <= 0) {
+            println("Only positive number")
+            return
+        }
+        var oldLevel = level
+        xp += amount
+        val newLevel = level
+
+        if (newLevel > oldLevel) {
+            println("Congrats! ${player}. Your level is increased to level ${newLevel}!")
+        }
+    }
 }
