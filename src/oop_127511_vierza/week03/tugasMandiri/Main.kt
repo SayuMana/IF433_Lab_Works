e65371ebdab050361dba3c3bfc4d635e92777f10
SayuMana <vierzasyahputra@gmail.com>
2026-02-18 15:13:03 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_127511_vierza/week03/tugasMandiri/Main.kt
+++ src/oop_127511_vierza/week03/tugasMandiri/Main.kt
@@ -1,2 +1,12 @@
 package oop_127511_vierza.week03.tugasMandiri
 
+import com.sun.tools.javac.code.TypeAnnotationPosition.field
+
+fun main() {
+    var weapon = Weapon()
+
+    weapon.damage = -50
+    println(weapon.damage)
+    weapon.damage = 1000
+    println(weapon.damage)
+}
--- src/oop_127511_vierza/week03/tugasMandiri/Weapon.kt
+++ src/oop_127511_vierza/week03/tugasMandiri/Weapon.kt
@@ -1,4 +1,20 @@
 package oop_127511_vierza.week03.tugasMandiri
 
 class Weapon {
+    var name:String = "Default"
+    var damage:Int = 1
+        set(value) {
+            if (value <= 0) {
+                println("Damage must be greater than 0, set to default ($damage)")
+            } else if (value >= 1000) {
+                field = value
+                println("Damage too overpowered (max 1000). Set to 1000")
+            }
+        }
+    var tier:String = "common"
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
 }
