f36b2fb94f1b9de61f65be0d73b3f8cc4a27a2c0
SayuMana <vierzasyahputra@gmail.com>
2026-02-24 18:16:03 +0700

    week04: (task 2) test Employee hierarcy in main

--- src/oop_127511_vierza/week04/tugasMandiri2/Main.kt
+++ src/oop_127511_vierza/week04/tugasMandiri2/Main.kt
@@ -1,8 +1,16 @@
 package oop_127511_vierza.week04.tugasMandiri2
 
 fun main() {
-    var manager = Manager()
-    var developer = Developer()
+    var manager = Manager("Hoshino", 1000000)
+    var developer = Developer("Kisaki", 3000000, "C#")
 
+    println("\nManager")
+    manager.work()
+    print("Current salary: ${manager.baseSalary}, ")
+    print("Bonus: ${manager.calculateBonus()}")
 
+    println("\n\nDeveloper")
+    developer.work()
+    print("Current salary: ${developer.baseSalary}, ")
+    print("Bonus: ${developer.calculateBonus()}")
 }
