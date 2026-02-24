531dd3a7be18409055f4e32333fb479969a5dbd4
SayuMana <vierzasyahputra@gmail.com>
2026-02-24 18:08:26 +0700

    week04: (task 2) Developer class fix

--- src/oop_127511_vierza/week04/tugasMandiri2/Developer.kt
+++ src/oop_127511_vierza/week04/tugasMandiri2/Developer.kt
@@ -1,8 +1,6 @@
 package oop_127511_vierza.week04.tugasMandiri2
 
-class Developer() : Employee() {
-    val programmingLanguage: String
-
+class Developer(name: String, baseSalary: Int, val programmingLanguage: String) : Employee(name, baseSalary) {
     override fun work() {
         println("$name is now coding with $programmingLanguage")
     }
