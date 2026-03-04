4b6e48fedd2d4f3ce17ba0f8858e7eb784dfeec9
SayuMana <vierzasyahputra@gmail.com>
2026-03-04 08:34:42 +0700

    week05: implement polymorphic collectin and smart casting

---
+++ src/oop_127511_vierza/week05/Main.kt
@@ -0,0 +1,26 @@
+package oop_127511_vierza.week05
+
+fun main() {
+    val dosen1 = Dosen("Takanashi Hoshino", "06767")
+    val admin1 = Admin("Sorasaki Hina")
+
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("Aktivitas Pegawai")
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+
+        when (pegawai) {
+            is Dosen -> {
+                println("=> Dosen Terdeteksi (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
+            }
+
+            is Admin -> {
+                println("=> Admin terdeteksi")
+                pegawai.doAdminWork()
+            }
+        }
+        println("-------------------------------")
+    }
+}
