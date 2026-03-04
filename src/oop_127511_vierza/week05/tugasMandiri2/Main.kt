5a352a44d6d3926d1e521b83b31e8ce492aeacab
SayuMana <vierzasyahputra@gmail.com>
2026-03-04 09:43:55 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_127511_vierza/week05/tugasMandiri2/Main.kt
+++ src/oop_127511_vierza/week05/tugasMandiri2/Main.kt
@@ -1,2 +1,14 @@
 package oop_127511_vierza.week05.tugasMandiri2
 
+fun main() {
+    val ovo = EWallet("Hoshino", 50000.0)
+    val visa = CreditCard("Hina", 100000.0)
+
+    val payments: List<PaymentMethod> = listOf(ovo, visa)
+
+    for (paymentMethod in payments) {
+        println("Processing payment for ${paymentMethod.accountName}")
+        paymentMethod.processPayment(75000.0)
+        println("---------------")
+    }
+}
