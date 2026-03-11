7246b239b5d92e1b535d66f3f62ff72c9bcf8388
SayuMana <vierzasyahputra@gmail.com>
2026-03-11 11:36:11 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_127511_vierza/week06/tugasMandiri/Main.kt
@@ -0,0 +1,7 @@
+package oop_127511_vierza.week06.tugasMandiri
+
+fun main() {
+    val smartLamp = SmartLamp("1", "Ruang Tamu")
+    val smartSpeaker = SmartSpeaker("1", "Google Nest Dapur", "XaleidoscopiX")
+    val smartCCTV = SmartCCTV("1", "Ezviz Garasi")
+}
