c5e20441e8f6cfdd45af7113b8e0662abda4218b
SayuMana <vierzasyahputra@gmail.com>
2026-03-25 08:34:43 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_127511_vierza/week07/tugasMandiri/GameModels.kt
@@ -0,0 +1,10 @@
+package oop_127511_vierza.week07.tugasMandiri
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(65),
+    UNCOMMON(20),
+    RARE(10),
+    EPIC(4),
+    LEGENDARY(1)
+}
+
