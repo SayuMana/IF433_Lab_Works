3e9e2952fba94fb900c802f36eaaadb6ec613ad5
SayuMana <vierzasyahputra@gmail.com>
2026-03-25 08:06:45 +0700

    week07: (trial) demonstrate regular class equality failure

--- src/oop_127511_vierza/week07/Main.kt
+++ src/oop_127511_vierza/week07/Main.kt
@@ -8,4 +8,10 @@ fun main() {
     println("\n=== TEST COMPANION OBJECT ===")
     val client = NetworkClient.createClient()
     client.connect()
+
+    println("\n=== TEST REGULAR CLASS ===")
+    val req1 = RegularUser("Hoshino", 22)
+    val req2 = RegularUser("Hoshino", 22)
+    println(req1)
+    println("Sama? ${req1 == req2}")
 }
---
+++ src/oop_127511_vierza/week07/RegularUser.kt
@@ -0,0 +1,5 @@
+package oop_127511_vierza.week07
+
+class RegularUser (val name: String, val age: Int) {
+
+}
