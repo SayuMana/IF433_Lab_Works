0083f7861303e59ee7aca8742a504460420b2aa5
SayuMana <vierzasyahputra@gmail.com>
2026-04-14 21:12:00 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_127511_vierza/week08/tugasMandiri/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_127511_vierza.week08.tugasMandiri;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
