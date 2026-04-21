c14579ecde30afeda2f1a42cae41032f8ac477bf
SayuMana <vierzasyahputra@gmail.com>
2026-04-21 15:28:11 +0700

    week09: implement filter HOF to extract data

--- src/oop_127511_vierza/week09/latihan/HigherOrderFunctions.kt
+++ src/oop_127511_vierza/week09/latihan/HigherOrderFunctions.kt
@@ -4,6 +4,12 @@ fun main() {
     val numbers = listOf(1,2,3,4,5,6,7,8)
     println("Original numbers: $numbers")
     println("\nHOF filter")
-    val evens = numbers.filter { it % 2 == 0}
+    val evens = numbers.filter { it % 2 == 0 }
     println("Evens: $evens")
+
+    println("\nHOF map")
+    val multiplied = evens.map { it * 10 }
+    val asStrings = multiplied.map { "Rp ${it}k" }
+    println("Multiplied: $multiplied")
+    println("Formatted: $asStrings")
 }
