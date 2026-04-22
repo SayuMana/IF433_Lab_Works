57b63e111da64003d9a5ddba6f71277f1e7824f0
SayuMana <vierzasyahputra@gmail.com>
2026-04-22 08:06:24 +0700

    week09: (task) cretae TradeLog data class

---
+++ src/oop_127511_vierza/week09/tugasMandiri/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_127511_vierza.week09.tugasMandiri
+
+data class TaradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double, // Return on Equity
+    val status: String
+)
