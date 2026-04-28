e5f716b6d1d122847617b5cad12f9f630eea608b
SayuMana <vierzasyahputra@gmail.com>
2026-04-28 15:09:42 +0700

    week10: implement PairBox in main function

--- src/oop_127511_vierza/week10/Main.kt
+++ src/oop_127511_vierza/week10/Main.kt
@@ -4,7 +4,10 @@ fun main() {
     println("Test generic class")
     val intBox = Box(100)
     val stringBox = Box("Hello")
-
     println(intBox.value)
     println(stringBox.value)
+
+    println("\nTest multiple parameters")
+    val itemPrice = PairBox("Bitcoin", 65000)
+    println("Aset: ${itemPrice.key}, Harga: ${itemPrice.value} USD")
 }
