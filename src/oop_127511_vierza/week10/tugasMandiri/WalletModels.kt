3c4f8eb32141dc3593985d39f21560fe12628088
SayuMana <vierzasyahputra@gmail.com>
2026-04-28 16:57:16 +0700

    week10: (task) create Coin and Transaction

---
+++ src/oop_127511_vierza/week10/tugasMandiri/WalletModels.kt
@@ -0,0 +1,11 @@
+package oop_127511_vierza.week10.tugasMandiri
+
+data class Coin(
+    val name: String,
+    val balance: Double
+)
+
+data class Transaction(
+    val id: String,
+    val amount: Double
+)
