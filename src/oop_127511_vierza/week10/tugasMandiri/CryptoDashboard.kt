0f90d9def54bd19ccadbf1f69b731db632dd72bb
SayuMana <vierzasyahputra@gmail.com>
2026-04-29 08:32:42 +0700

    week10: (task) populate coin repository

--- src/oop_127511_vierza/week10/tugasMandiri/CryptoDashboard.kt
+++ src/oop_127511_vierza/week10/tugasMandiri/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_127511_vierza.week10.tugasMandiri
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 250.0))
 }
