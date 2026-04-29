a4c552eebbb9c103ff8dede4f421a09e040d8978
SayuMana <vierzasyahputra@gmail.com>
2026-04-29 08:46:52 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_127511_vierza/week10/tugasMandiri/CryptoDashboard.kt
+++ src/oop_127511_vierza/week10/tugasMandiri/CryptoDashboard.kt
@@ -7,9 +7,12 @@ fun main() {
     coinRepo.add(Coin("USDT", 250.0))
 
     val response = ApiResponse("200 OK", coinRepo.getAll())
-
     println("Status : ${response.status}")
     response.data.forEach { coin ->
         println("Nama: ${coin.name}, Saldo: ${coin.balance}")
     }
+
+    val txRepo = WalletRepository<Transaction>()
+    txRepo.add(Transaction("TX-9901A", 500.0))
+    txRepo.add(Transaction("TX-9902B", 125.5))
 }
