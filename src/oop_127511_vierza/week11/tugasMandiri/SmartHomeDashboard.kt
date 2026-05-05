5e1896fd20d08ca0b9ad1eeb6fd5614fc76877dc
SayuMana <vierzasyahputra@gmail.com>
2026-05-05 14:42:26 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_127511_vierza/week11/tugasMandiri/SmartHomeDashboard.kt
+++ src/oop_127511_vierza/week11/tugasMandiri/SmartHomeDashboard.kt
@@ -6,8 +6,15 @@ fun main() {
         isOnline = true
         powerLoad = 12
     }.also {
-        homeDevices.add(it)
         println("Data Baru: $it")
+        homeDevices.add(it)
     }
 
+    SmartDevice("Ezviz Outdoor", "Camera").apply {
+        isOnline = true
+        powerLoad = 5
+    }.also {
+        println("(LOG) Kamera terhubung")
+        homeDevices.add(it)
+    }
 }
