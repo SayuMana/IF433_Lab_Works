2b1b16d0f4888894ee8131ef5377366d9f2e0fa2
SayuMana <vierzasyahputra@gmail.com>
2026-05-05 14:11:58 +0700

    week11: implement apply for object initialization

--- src/oop_127511_vierza/week11/latihan/ScopeConfiguration.kt
+++ src/oop_127511_vierza/week11/latihan/ScopeConfiguration.kt
@@ -1,3 +1,12 @@
 package oop_127511_vierza.week11.latihan
 
 data class User(var name: String = "", var age: Int = 0)
+
+fun main() {
+    println("Test apply")
+    val user = User().apply {
+        name = "Hoshino"
+        age = 17
+    }
+    println(user)
+}
