9c359c4049161b1e30250c5748ae4648bc740f4d
SayuMana <vierzasyahputra@gmail.com>
2026-05-05 14:13:55 +0700

    week11: implement also for logging side effects

--- src/oop_127511_vierza/week11/latihan/ScopeConfiguration.kt
+++ src/oop_127511_vierza/week11/latihan/ScopeConfiguration.kt
@@ -3,10 +3,17 @@ package oop_127511_vierza.week11.latihan
 data class User(var name: String = "", var age: Int = 0)
 
 fun main() {
-    println("Test apply")
+    println("\nTest apply")
     val user = User().apply {
         name = "Hoshino"
         age = 17
     }
     println(user)
+
+    println("\nTest also")
+    val numbers = mutableListOf(1, 2, 3)
+    numbers.also {
+        println("Log sebelum ditambah: $it")
+    }.add(4)
+    println("Setelah ditambah: $numbers")
 }
