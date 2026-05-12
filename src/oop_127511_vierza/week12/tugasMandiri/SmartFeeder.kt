b649e0e2dfe5d64d3822c5e5cfff87cd121d3845
SayuMana <vierzasyahputra@gmail.com>
2026-05-12 13:07:26 +0700

    week12: (task) implement finally block for feeding logging

--- src/oop_127511_vierza/week12/tugasMandiri/SmartFeeder.kt
+++ src/oop_127511_vierza/week12/tugasMandiri/SmartFeeder.kt
@@ -23,5 +23,7 @@ fun main() {
         println("Error: ${e.message}")
     } catch (e: Exception) {
         println("Error: ${e.message}")
+    } finally {
+        println("Siklus pengecekan dispenser pagi selesai")
     }
 }
