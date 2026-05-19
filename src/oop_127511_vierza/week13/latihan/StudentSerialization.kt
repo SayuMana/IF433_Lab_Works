68bdcfe304a367f4a965999d26daca860a13bf5d
SayuMana <vierzasyahputra@gmail.com>
2026-05-19 13:33:28 +0700

    week13: create save and load list functions

--- src/oop_127511_vierza/week13/latihan/StudentSerialization.kt
+++ src/oop_127511_vierza/week13/latihan/StudentSerialization.kt
@@ -17,3 +17,18 @@ fun fromCSV(line: String) : Student{
     return Student(parts[0], parts[1].toInt(), parts[2].toDouble())
 }
 
+fun saveStudent(student: List<Student>, path: String) {
+    File(path).writeText(
+        student.joinToString("\n") {
+        it.toCSV()
+    })
+}
+
+fun loadStudents(path: String) : List<Student>{
+    return try {
+        File(path).readLines().map { fromCSV(it) }
+    } catch (e: FileNotFoundException) {
+        println("Error: File tidak ditemukan!")
+        emptyList()
+    }
+}
