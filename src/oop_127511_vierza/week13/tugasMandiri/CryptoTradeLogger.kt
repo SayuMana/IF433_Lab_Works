24e0ca54429468033c620ba35fa0d8873e785c0f
SayuMana <vierzasyahputra@gmail.com>
2026-05-19 13:56:10 +0700

    week13: (task) implement fromCSVTrade with split

--- src/oop_127511_vierza/week13/tugasMandiri/CryptoTradeLogger.kt
+++ src/oop_127511_vierza/week13/tugasMandiri/CryptoTradeLogger.kt
@@ -12,12 +12,17 @@ fun TradeRecord.toCSV(): String =
     "$id,$symbol,$type,$margin,$pnl"
 
 fun fromCSVTrade(line: String) : TradeRecord?{
-    val parts = line.split(",")
-    return TradeRecord(
-        parts[0].toInt(),
-        parts[1],
-        parts[2],
-        parts[3].toDouble(),
-        parts[4].toDouble()
-    )
+    return try {
+        val parts = line.split(",")
+        return TradeRecord(
+            parts[0].toInt(),
+            parts[1],
+            parts[2],
+            parts[3].toDouble(),
+            parts[4].toDouble()
+        )
+    } catch (e: Exception) {
+        println("Log: Data korup diabaikan: $line")
+        null
+    }
 }
