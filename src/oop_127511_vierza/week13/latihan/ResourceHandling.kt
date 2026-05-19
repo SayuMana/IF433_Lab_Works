86bab14816c5cd66626ec5f7471221f38f974031
SayuMana <vierzasyahputra@gmail.com>
2026-05-19 13:18:48 +0700

    week13: implement printWriter with safe use block

--- src/oop_127511_vierza/week13/latihan/ResourceHandling.kt
+++ src/oop_127511_vierza/week13/latihan/ResourceHandling.kt
@@ -2,7 +2,7 @@ package oop_127511_vierza.week13.latihan
 import java.io.File
 
 fun main() {
-    println("Test unsafe resource handling")
+    println("\nTest unsafe resource handling")
     val unsafeFile = File("unsafeLogs.txt")
     val writer = unsafeFile.printWriter()
 
@@ -10,4 +10,13 @@ fun main() {
     writer.println("Log 2: Menulis data pengguna...")
     writer.close()
     println("Proses penulisan unsafe selesai")
+
+    println("\nTest safe resource handling")
+    val safeFile = File("safeLogs.txt")
+    safeFile.printWriter().use { out ->
+        for (i in 1..100) {
+            out.println("Safe Log entry #$i: System status OK")
+        }
+        println("100 baris log berhasil di generate dengan aman")
+    }
 }
