bcc88493f5fac5b4764b54f98661212b41059447
SayuMana <vierzasyahputra@gmail.com>
2026-05-19 13:25:06 +0700

    week13: create Student model

---
+++ src/oop_127511_vierza/week13/latihan/StudentSerialization.kt
@@ -0,0 +1,9 @@
+package oop_127511_vierza.week13.latihan
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(
+    val name: String,
+    val age: Int,
+    val gpa: Double
+)
