df3420833dfe5050741d20cfa9881f7ee7bfad8a
SayuMana <vierzasyahputra@gmail.com>
2026-05-19 13:27:35 +0700

    week13: implement toCSV and from CSV logic

--- src/oop_127511_vierza/week13/latihan/StudentSerialization.kt
+++ src/oop_127511_vierza/week13/latihan/StudentSerialization.kt
@@ -7,3 +7,13 @@ data class Student(
     val age: Int,
     val gpa: Double
 )
+
+// Serialization (Object to CSV)
+fun Student.toCSV(): String = "$name, $age, $gpa"
+
+// Deseletialization (CSV to Object)
+fun fromCSV(line: String) : Student{
+    val parts = line.split(",")
+    return Student(parts[0], parts[1].toInt(), parts[2].toDouble())
+}
+
