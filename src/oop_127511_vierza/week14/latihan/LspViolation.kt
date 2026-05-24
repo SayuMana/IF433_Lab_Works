9ccf9610487b274daeaa76d1e2b72ebbba5b4aff
SayuMana <vierzasyahputra@gmail.com>
2026-05-24 18:34:59 +0700

    create bad Square extending Rectangle (LSP violation)

---
+++ src/oop_127511_vierza/week14/latihan/LspViolation.kt
@@ -0,0 +1,21 @@
+package oop_127511_vierza.week14.latihan
+
+open class Rectangle {
+    open var width: Int = 0
+    open var height: Int = 0
+    fun area() = width * height
+}
+
+class Square: Rectangle() {
+    override var width: Int = 0
+        set(value) {
+            field = value;
+            super.height = value
+        }
+
+    override var height: Int = 0
+        set(value) {
+            field = value;
+            super.height = value
+        }
+}
