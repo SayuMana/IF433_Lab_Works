ba330f3b9f93ce006b53ee93129b7b83a7690f78
SayuMana <vierzasyahputra@gmail.com>
2026-05-24 18:38:11 +0700

    implement Shape independently to fix LSP

--- src/oop_127511_vierza/week14/latihan/LspRefactored.kt
+++ src/oop_127511_vierza/week14/latihan/LspRefactored.kt
@@ -3,3 +3,11 @@ package oop_127511_vierza.week14.latihan
 interface Shape {
     fun area(): Int
 }
+
+class SafeRectangle(val width: Int, val height: Int) : Shape {
+    override fun area() = width * height
+}
+
+class SafeSquare(var side: Int) : Shape {
+    override fun area() = side * side
+}
