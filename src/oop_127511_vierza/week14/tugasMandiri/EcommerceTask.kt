e919d15ea5b893e224d9c79b8acdc92554662434
SayuMana <vierzasyahputra@gmail.com>
2026-05-24 19:05:13 +0700

    week 14: (task) copy bad E-Commerce OrderProcessor code

---
+++ src/oop_127511_vierza/week14/tugasMandiri/EcommerceTask.kt
@@ -0,0 +1,19 @@
+package oop_127511_vierza.week14.tugasMandiri
+import java.io.File
+
+class BadOrderProcessor {
+    private val file = File("src/oop_127511_vierza/week14/tugasMandiri/orders.csv") //DIP
+
+    fun processOrder(itemName: String, basePrice:Double, customerType: String) {
+        val finalPrice = when (customerType) {
+            "REGULAR" -> basePrice
+            "VIP" -> basePrice * 0.9
+            else -> basePrice
+        } // ocp
+
+        println("Memproses pesanan $itemName seharga $finalPrice")
+
+        file.appendText("$itemName,$finalPrice,$customerType\n") //srp dip
+        println("Email terkirim! Pesanan $itemName Anda telah dikonfirmansi!") // srp dip
+    }
+}
